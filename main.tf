3286e4244379a04e80f9676763c7a9e0852490df
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-12-14 10:03:35 +0530

    GCP -API enabler -org level

---
+++ main.tf
@@ -0,0 +1,50 @@
+# Terraform file for the creation of service account and providing access
+
+#Header start
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+#Header End
+# locals {
+#   checkrole = var.role_id == null ? 0:1
+# }
+#data collector 
+data "google_projects" "my-org-projects" {
+  filter = "parent.id:${var.org_id}"
+}
+
+# resource for making a custom role from the set of permission
+# resource "google_organization_iam_custom_role" "my-custom-role" {
+#   count = local.checkrole
+#   role_id     = var.role_id
+#   org_id      = var.org_id
+#   title       = "custom-role-org-core-ag"
+#   description = "Custom role for the corestack gcp module"
+#   permissions = var.permissionsec == null ? var.permission_nosecops : var.permission_full
+# }
+# # resource for assigning the custom role to the service account 
+# resource "google_organization_iam_member" "binding_custom_role" {
+#   for_each = var.assign_role
+#   org_id = var.org_id
+#   role   = var.role_id == null ? "roles/${each.value}" : "organizations/${var.org_id}/roles/${var.role_id}"
+#   member = "serviceAccount:${var.service_account_email}"
+#   depends_on = [
+#     google_organization_iam_custom_role.my-custom-role
+#   ]
+# }
+
+# Resource to execute the python script that will enable the apis. 
+resource "null_resource" "Project_script" {
+ provisioner "local-exec" {  
+    command = "/bin/python3 extractor.py"
+  }
+  depends_on = [
+    google_organization_iam_member.binding_custom_role
+  ]
+}
+
---
+++ run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+echo "Please ensure you have the necessary IAM permissions .The execution of this script requires following user account permissions: "
+echo "- Organization level Security Admin Role"
+echo "- Service Usage Admin"
+read -p "Please specify yes if above pre-requisites met or specify no : " INPUT
+if [ -f vars.tfvars ]; then
+  echo vars.tfvars exists.
+  terraform init
+  terraform apply -var-file="vars.tfvars" -auto-approve
+  echo "Approved"
+else
+if [ "${INPUT}" == "No" ] || [ "${INPUT}" == "no" ] ; then
+     # exiting with return code 0
+   echo 'Exiting..'
+   exit 0 
+elif [ "${INPUT}" == "Yes" ]||[ "${INPUT}" == "yes" ] ; then   
+read -p "Please enter the Organisation id: " org_id
+read -p "Please enter the service account email: " svcacc
+echo "Assign predefined role?"
+echo "- roles/editor"
+echo "- roles/pubsub.admin"
+echo "- roles/securitycenter.adminEditor"
+echo "- roles/monitoring.editor"
+echo "- roles/logging.configWriter"
+echo "- roles/compute.admin"
+read -p "Please specify yes or no: " role
+  if [ "${role}" == "No" ] || [ "${role}" == "no" ] ; then
+  read -p "Do you need to retrive vunrelabilities and threat from GCP console? specify yes or no:" secops
+  read -p "Creating custom role, provide the name: " roleid
+  if [ "${secops}" == "No" ] || [ "${secops}" == "no" ] ; then
+  cat <<EOF > ./vars.tfvars
+org_id = "$org_id"
+service_account_email = "$svcacc"
+role_id = "$roleid"
+api = ["cloudresourcemanager.googleapis.com" ,"compute.googleapis.com","recommender.googleapis.com", "securitycenter.googleapis.com", "orgpolicy.googleapis.com", "sqladmin.googleapis.com", "monitoring.googleapis.com", "pubsub.googleapis.com"]
+EOF
+  elif [ "${secops}" == "yes" ] || [ "${secops}" == "Yes" ]; then
+ cat <<EOF > ./vars.tfvars
+org_id = "$org_id"
+service_account_email = "$svcacc"
+role_id = "$roleid"
+permissionsec = "yes"
+api = ["cloudresourcemanager.googleapis.com" ,"compute.googleapis.com","recommender.googleapis.com", "securitycenter.googleapis.com", "orgpolicy.googleapis.com", "sqladmin.googleapis.com", "monitoring.googleapis.com", "pubsub.googleapis.com"]
+EOF
+fi
+  elif [ "${role}" == "Yes" ]||[ "${role}" == "yes" ] ; then
+  echo "granting predefined roles:"
+  cat <<EOF > ./vars.tfvars
+org_id = "$org_id"
+service_account_email = "$svcacc"
+api = ["cloudresourcemanager.googleapis.com" ,"compute.googleapis.com","recommender.googleapis.com", "securitycenter.googleapis.com", "orgpolicy.googleapis.com", "sqladmin.googleapis.com", "monitoring.googleapis.com", "pubsub.googleapis.com"]
+EOF
+fi
+terraform init
+terraform apply -var-file="vars.tfvars" -auto-approve
+echo "Approved"
+fi
+fi
